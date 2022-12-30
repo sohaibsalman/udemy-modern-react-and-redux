6b27cec5af4a9d8169a756c28a3ec4b489059d51
sohaibsalman <sohaibsalman10@gmail.com>
2022-12-30 19:56:04 +0500

    Add redux toolkit query endpoint to add album

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,5 +1,40 @@
+import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
+import Button from "../components/Button";
+import Skeleton from "../components/Skeleton";
+import ExpandablePanel from "../components/ExpandablePanel";
+
 function AlbumsList({ user }) {
-  return <div>Albums list for {user.name}</div>;
+  const { data, isLoading, error } = useFetchAlbumsQuery(user);
+  const [addAlbum, result] = useAddAlbumMutation();
+
+  const handleClick = () => {
+    addAlbum(user);
+  };
+
+  let content;
+  if (isLoading) {
+    content = <Skeleton times={3} className="w-full h-10" />;
+  } else if (error) {
+    content = <div>Error fetching albums!</div>;
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>;
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          List of photos for {album.title}
+        </ExpandablePanel>
+      );
+    });
+  }
+
+  return (
+    <div>
+      <Button className="mb-2" onClick={handleClick}>
+        + Add Album
+      </Button>
+      {content}
+    </div>
+  );
 }
 
 export default AlbumsList;
--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { faker } from "@faker-js/faker";
 
 const albumsApi = createApi({
   // reducerPath will be set as the key of the state object in store
@@ -13,6 +14,18 @@ const albumsApi = createApi({
   // describes how we will fetch data from server
   endpoints: (builder) => {
     return {
+      addAlbum: builder.mutation({
+        query: (user) => {
+          return {
+            url: "/albums",
+            method: "POST",
+            body: {
+              title: faker.commerce.productName(),
+              userId: user.id,
+            },
+          };
+        },
+      }),
       fetchAlbums: builder.query({
         query: (user) => {
           return {
@@ -26,5 +39,5 @@ const albumsApi = createApi({
   },
 });
 
-export const { useFetchAlbumsQuery } = albumsApi;
+export const { useFetchAlbumsQuery, useAddAlbumMutation } = albumsApi;
 export { albumsApi };
