e27a8361c3a08e790fa2a99610c81871ed5d84dc
sohaibsalman <sohaibsalman10@gmail.com>
2022-12-30 13:29:45 +0500

    Create list item component

--- src/store/thunks/removeUser.js
+++ src/store/thunks/removeUser.js
@@ -4,7 +4,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { config } from "../../config";
 
 const removeUser = createAsyncThunk("users/remove", async (user) => {
-  await axios.delete(`${config.API_URL}/users/${user}`);
+  await axios.delete(`${config.API_URL}/users/${user.id}`);
   return user;
 });
 
